e388625f422957353634b1dd43af41d524748871
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-06 19:44:22 +0700

    1

--- java-core/day_03/SwitchCase/src/Main.java
+++ java-core/day_03/SwitchCase/src/Main.java
@@ -3,13 +3,64 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /*
         System.out.println("Nhập số :");
         int number = sc.nextInt();
 
-        switch (number) {
+        switch(number){
+            case 1:
+                System.out.println("One");
+                break;
             case 2:
-                System.out.println("Thứ 2");
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            default:
+                System.out.println("Không có lựa chọn này");
+        }
+         */
+        System.out.println("Nhập tháng :");
+        int month = sc.nextInt();
 
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                System.out.println("Tháng "+month+" có 31 ngày.");
+                break;
+            case 2:
+                System.out.println("Tháng "+month+" có 28 hoặc 29 ngày.");
+                break;
+            case 4, 6, 9, 11:
+                System.out.println("Tháng "+month+" có 30 ngày.");
+                break;
+            default:
+                System.out.println("Không có tháng này.");
+        }
+
+        System.out.println("Lựa chọn bạn là: ");
+        System.out.println("1 - Xem tất cả sản phẩm");
+        System.out.println("2 - Tìm sản phẩm theo tên");
+        System.out.println("3 - Xem giỏ hàng");
+        System.out.println("4 - Thoát chương trình");
+        int x = sc.nextInt();
+
+        switch (x) {
+            case 1:
+                System.out.println("Danh sách sản phẩm");
+                System.out.println("....");
+                break;
+            case 2:
+                System.out.println("etc");
+                break;
+            default:
+                System.out.println("Không có lựa chọn này.");
         }
     }
 }
