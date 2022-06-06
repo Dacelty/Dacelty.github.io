c588e52be334927756a1d6ccf1ac7884ca6259b1
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-06 19:23:19 +0700

    1

--- java-core/day_03/SwitchCase/src/Main.java
+++ java-core/day_03/SwitchCase/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập số :");
+        int number = sc.nextInt();
+
+        switch (number) {
+            case 2:
+                System.out.println("Thứ 2");
+
+        }
     }
 }
