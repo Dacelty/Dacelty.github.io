7b12960926983fe6f3188fe4c04749898480037f
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-06 20:31:20 +0700

    1

--- java-core/day_03/ForLoop/src/Main.java
+++ java-core/day_03/ForLoop/src/Main.java
@@ -7,7 +7,25 @@ public class Main {
                 System.out.println(i+" Fizz");
             } else if (i%5==0) {
                 System.out.println(i+" Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        for (int i = 0; i <= 100; i++){
+            if (i % 3 == 0){
+                if (i % 5 == 0){
+                    System.out.println("fizzbuzz");
+                }else {
+                    System.out.println("fizz");
+                }
+            } else if(i % 5 ==0){
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
             }
         }
     }
+
+
 }
