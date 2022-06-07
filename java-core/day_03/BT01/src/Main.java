c699a8c35ab39d10925909409e8f7fd56fa7cae0
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-07 22:39:37 +0700

    1

--- java-core/day_03/BT01/src/Main.java
+++ java-core/day_03/BT01/src/Main.java
@@ -9,13 +9,15 @@ public class Main {
         System.out.println("String đã nhập là :");
         System.out.println(hello);
 
+        System.out.println("Ký tự e xuất hiện ở index: ");
         for (int i = 0; i < hello.length(); i++) {
             if (hello.charAt(i) == someChar) {
-                System.out.println("Ký tự e xuất hiện ở index "+i+".");
+                System.out.printf(i+"\t");
                 count++;
             }
         }
 
+        System.out.println("\n");
         System.out.println("Ký tự e xuất hiện "+count+" lần.");
     }
 }
