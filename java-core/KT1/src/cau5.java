a5bc6cc359aadcc5b036ca0e9c60fabc97ff11ab
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-10 20:40:22 +0700

    1

--- java-core/KT1/src/cau5.java
+++ java-core/KT1/src/cau5.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+
 public class cau5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row,col;
+        System.out.println("Nhập số hàng của mảng :");
+        row = scanner.nextInt();
+        System.out.println("Nhập số cột của mảng :");
+        col = scanner.nextInt();
+
+        int[][] array = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("Nhập phần tử thứ [" + i + ", " + j + "]: ");
+                array[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Mảng vừa nhập: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+
+        int[][] arrayB = new int[row][col];
+        System.out.println("Đã tạo thêm mảng mới có cùng số dòng và cột.");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("Nhập phần tử thứ [" + i + ", " + j + "] của mảng 2: ");
+                arrayB[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Mảng 2 vừa nhập: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arrayB[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+
+        int[][] arrayC = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arrayC[i][j] = array[i][j]+arrayB[i][j];
+            }
+        }
+
+        System.out.println("Cộng hai mảng thì sẽ cho ra mảng mới là: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arrayC[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+
+    }
 }
