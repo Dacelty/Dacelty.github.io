8f4a64e48d81915bd51c54d2be8b456b978fdbab
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-15 19:37:34 +0700

    1

--- java-core/day_07/src/Main.java
+++ java-core/day_07/src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+//        p1.name = "Hoa";
+//        p1.age =20;
+//        p1.address = "Hà Nội";
+//
+//        System.out.println(p1.name + " - " +p1.age + " - " +p1.address);
+        p1.setName("Hoa");
+        p1.setAge(20);
+        p1.setAddress("Hà Nội");
+
+        System.out.println(p1.getName() + " - " +p1.getAge() + " - " +p1.getAddress());
+
+        Person p2 = new Person("Huy",22,"Hà Nội");
+        System.out.println(p2.getName() + " - " +p2.getAge() + " - " +p2.getAddress());
+        p2.setAge(25);
+        System.out.println(p2.getName() + " - " +p2.getAge() + " - " +p2.getAddress());
+    }
 }
