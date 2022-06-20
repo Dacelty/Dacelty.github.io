d78acd40da06a5d2a17390e7bed75f3da86e260b
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-06-20 09:17:44 +0700

    1

---
+++ java-core/day_08/BT/src/Movie.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Movie extends Film {
+    private double length;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String name, String genre, String director, LocalDate dob, double length) {
+        super(id, name, genre, director, dob);
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public void myMethod() {
+        super.myMethod();
+        System.out.println("Lớp con");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - " + length;
+    }
+}
