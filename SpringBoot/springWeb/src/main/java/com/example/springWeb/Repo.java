80a0ef15701936ff6c19a2c81bc0f34f82361d5d
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-09-08 00:09:18 +0700

    1

--- SpringBoot/springWeb/src/main/java/com/example/springWeb/Repo.java
+++ SpringBoot/springWeb/src/main/java/com/example/springWeb/Repo.java
@@ -25,7 +25,10 @@ public class Repo {
             File file = ResourceUtils.getFile("src/main/resources/MOCK_DATA.json");
             ObjectMapper mapper = new ObjectMapper();
             car.addAll(mapper.readValue(file, new TypeReference<List<Car>>() {}));
-            car.forEach(System.out::println);
+//            car.forEach(System.out::println);
+            for (Car c:car){
+                System.out.println(c.getBrand() + " - " + c.getModel());
+            }
             return car;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
