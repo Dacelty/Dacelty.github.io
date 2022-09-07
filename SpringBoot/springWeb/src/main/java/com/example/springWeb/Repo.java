870b278bc89d5a875ca386026fe8f25407e05700
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-09-07 23:34:16 +0700

    1

---
+++ SpringBoot/springWeb/src/main/java/com/example/springWeb/Repo.java
@@ -0,0 +1,40 @@
+package com.example.springWeb;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class Repo {
+    private ArrayList<Car> car = new ArrayList<>();
+
+
+
+
+    public ArrayList<Car> Repo(){
+        try {
+            File file = ResourceUtils.getFile("src/main/resources/MOCK_DATA.json");
+            ObjectMapper mapper = new ObjectMapper();
+            car.addAll(mapper.readValue(file, new TypeReference<List<Car>>() {}));
+            car.forEach(System.out::println);
+            return car;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (StreamReadException e) {
+            throw new RuntimeException(e);
+        } catch (DatabindException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
