7343936a41fbab4ab8dc4df7e275246bc0b27062
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-09-12 20:27:25 +0700

    1

---
+++ SpringBoot/springRestCrud/src/test/java/com/example/springRestCrud/SpringRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springRestCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
