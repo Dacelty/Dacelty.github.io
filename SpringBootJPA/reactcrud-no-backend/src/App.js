0f96c9bed1b8567e9410b8e0763187550882e54a
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-11-01 09:03:33 +0700

    1

---
+++ SpringBootJPA/reactcrud-no-backend/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
+
+function App() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Create />}></Route>
+          <Route path="/read" element={<Read />}></Route>
+          <Route path="/update" element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
