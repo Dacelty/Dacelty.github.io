eb05d19fbe207545b399df0c24dddb564182cb2d
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-11-04 18:38:38 +0700

    1

--- SpringBootJPA/kt-jpa-shoppingcart/src/index.js
+++ SpringBootJPA/kt-jpa-shoppingcart/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
