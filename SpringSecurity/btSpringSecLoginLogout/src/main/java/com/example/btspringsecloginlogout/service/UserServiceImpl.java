a91dabe64847d2e9f4bf1c6d739e219c98f59965
Dacelty <104076036+Dacelty@users.noreply.github.com>
2022-11-10 17:37:44 +0700

    1

--- SpringSecurity/btSpringSecLoginLogout/src/main/java/com/example/btspringsecloginlogout/service/UserServiceImpl.java
+++ SpringSecurity/btSpringSecLoginLogout/src/main/java/com/example/btspringsecloginlogout/service/UserServiceImpl.java
@@ -1,34 +1,34 @@
-//package com.example.btspringsecloginlogout.service;
-//
-//import com.example.btspringsecloginlogout.entity.User;
-//import com.example.btspringsecloginlogout.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import javax.persistence.EntityNotFoundException;
-//import java.util.Collections;
-//
-//@Service
-//public class UserServiceImpl implements UserService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        System.out.println("username");
-//        System.out.println(username);
-//        User user = userRepository.findByTaiKhoan(username);
-//        System.out.println(user);
-//
-//        if (user == null) {
-//            throw new EntityNotFoundException("Username or password not found");
-//        }
-//
-//        String password = user.getMatKhau();
-//
-//        return new org.springframework.security.core.userdetails.User(username, password, Collections.emptyList());
-//    }
-//}
+package com.example.btspringsecloginlogout.service;
+
+import com.example.btspringsecloginlogout.entity.User;
+import com.example.btspringsecloginlogout.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Collections;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("username");
+        System.out.println(username);
+        User user = userRepository.findByTaiKhoan(username);
+        System.out.println(user);
+
+        if (user == null) {
+            throw new EntityNotFoundException("Username or password not found");
+        }
+
+        String password = user.getMatKhau();
+
+        return new org.springframework.security.core.userdetails.User(username, password, Collections.emptyList());
+    }
+}
